0e527f7a1063eb93e32204875d32719654157a5a
Hyeon woo Kim <wooin.dev@gmail.com>
2023-11-16 17:36:01 +0900

    [fix] 퀴즈 열람 카운트가 동작되는 조건을 처음 조회시로 변경.
    - 기존에는 볼 때마다 카운트.

--- src/main/java/com/wooin/hahahaback/quizUserData/service/QuizUserDataService.java
+++ src/main/java/com/wooin/hahahaback/quizUserData/service/QuizUserDataService.java
@@ -7,6 +7,7 @@ import com.wooin.hahahaback.quizUserData.dto.QuizUserDataResponseDto;
 import com.wooin.hahahaback.quizUserData.entity.QuizUserData;
 import com.wooin.hahahaback.quizUserData.repository.QuizUserDataRepository;
 import com.wooin.hahahaback.user.entity.User;
+import com.wooin.hahahaback.userdata.service.UserDataService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +16,12 @@ public class QuizUserDataService {
 
     private final QuizUserDataRepository quizUserDataRepository;
     private final QuizRepository quizRepository;
+    private final UserDataService userDataService;
 
-    public QuizUserDataService(QuizUserDataRepository quizUserDataRepository, QuizRepository quizRepository) {
+    public QuizUserDataService(QuizUserDataRepository quizUserDataRepository, QuizRepository quizRepository, UserDataService userDataService) {
         this.quizUserDataRepository = quizUserDataRepository;
         this.quizRepository = quizRepository;
+        this.userDataService = userDataService;
     }
 
 
@@ -31,12 +34,13 @@ public class QuizUserDataService {
             Quiz foundQuiz = findQuizById(quizId);
             QuizUserData newQuizUserData = new QuizUserData(foundQuiz, user);
             quizUserData = quizUserDataRepository.save(newQuizUserData);
-//            throw new IllegalArgumentException("이미 조회 데이터가 생성되었습니다.");
+            //데이터가 없었다는 것은 처음 조회한것을 의미하므로.
+            userDataService.countShowQuiz(user);
         } else {
             quizUserData = findQuizUserData(user, quizId);
         }
+        if (quizUserData == null) throw new IllegalArgumentException("올바르지 않은 접근입니다.");
 
-        if (quizUserData==null) throw new IllegalArgumentException("올바르지 않은 접근입니다.");
 
         return new QuizUserDataResponseDto(quizUserData);
     }
@@ -45,16 +49,18 @@ public class QuizUserDataService {
     public void showHint(User user, Long quizId) {
         QuizUserData foundQuizUserData = findQuizUserData(user, quizId);
         foundQuizUserData.showHint();
+        userDataService.countShowHint(user);
     }
 
     @Transactional
     public void solveQuiz(User user, Long quizId) {
         QuizUserData foundQuizUserData = findQuizUserData(user, quizId);
         foundQuizUserData.solveQuiz();
+        userDataService.countSolveQuiz(user);
     }
 
     private QuizUserData findQuizUserData(User user, Long quizId) {
-        return quizUserDataRepository.findByUserAndQuiz_Id(user, quizId).orElseThrow(()->new NotFoundException("해당 퀴즈-유저 데이터가 없습니다"));
+        return quizUserDataRepository.findByUserAndQuiz_Id(user, quizId).orElseThrow(() -> new NotFoundException("해당 퀴즈-유저 데이터가 없습니다"));
     }
 
     private Quiz findQuizById(Long quizId) {
